3da76c81489c358e6ef1f6a8c6dc95a85fad73b7
Muhammad Atteq <muhammadatteq7866@gmail.com>
2025-06-02 09:05:06 -0700

    User api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB successfully!');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000 !!!');
 });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the API!'
+    });
+}
+);
+
+app.use('/api/users', userRoutes);
     
 
